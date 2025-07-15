f1ffe366b89527db2289cafb3ea507a90d9011bf
ratanjyoti <ratanjyotimgs13@gmail.com>
2025-07-15 14:18:13 +0530

    Add setup, requirements, and livesensor as submodule

---
+++ sensor/__init__.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sensor",
+version="0.0.1",
+author="ratan jyoti",
+packages= find_packages(),
+)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+def get_requirements()->list[str]:
+    requirements_list=list[str]=[]
+
+    return requirements_list
+
+setup(
+    name ='sensor',
+    author ="ratan jyoti",
+    version ="0.0.1",
+    packages = find_packages(),
+    install_require = get_requirements(),
+)
